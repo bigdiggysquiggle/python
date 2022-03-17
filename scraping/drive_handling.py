abea52de58e2a300b5429ff6e2324628b3a90bdf
bigdiggysquiggle <dylanromansky@yahoo.com>
2022-03-17 12:24:40 -0700

    added functionality to the scraper which uploads the job description files into folders in my google drive organized by date and title of the job listing

---
+++ scraping/drive_handling.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+from datetime import date
+import os
+
+def drive_setup(SCOPES):
+	if not SCOPES:
+		raise Exception("No scope")
+	creds = None
+	if os.path.exists('token.json'):
+		creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+			creds = flow.run_local_server(port=0)
+		with open('token.json', 'w') as token:
+			token.write(creds.to_json())
+	return build('drive', 'v3', credentials = creds)
+
+def drive_fill(DRIVE_DIR, DRIVE):
+	print('from: ' + str(date.today()))
+	DEST_META = {'name': str(date.today()), 'mimeType': "application/vnd.google-apps.folder", 'parents': [DRIVE_DIR['files'][0]['id']]}
+	DEST = DRIVE.files().create(body=DEST_META, fields="id").execute()
+	ID = DEST.get('id')
+	for file in os.listdir():
+		print('upload: ' + file)
+		META = {"name": file, "parents": [ID]}
+		DATA = MediaFileUpload(file)
+		UPLOAD = DRIVE.files().create(body=META, media_body=DATA, fields='id').execute()
--- scraping/get_a_job.py
+++ scraping/get_a_job.py
@@ -4,15 +4,28 @@
 #complete new scripts
 
 import os
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 import linkedin
+import drive_handling
+from datetime import date
 
+SCOPES = ['https://www.googleapis.com/auth/drive']
 start_dir = '/home/dromansk/projects/Python/scraping/'
+
 
 #create our output directories if they don't exist
 os.chdir(start_dir)
+drive = drive_handling.drive_setup(SCOPES)
+drive_dir = drive.files().list(q='name = "job descriptions"').execute()
 if os.path.exists('output') == False:
 	os.mkdir('output')
 os.chdir('output')
 if os.path.exists('linkedin') == False:
 	os.mkdir('linkedin')
-linkedin.crawl_linkedin()
+#linkedin.crawl_linkedin()
+os.chdir(start_dir + '/output/linkedin/' + str(date.today()))
+drive_handling.drive_fill(drive_dir, drive)
