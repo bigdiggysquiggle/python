aae1467fb409f2e5b75f1da967b0228ab60f3b82
bigdiggysquiggle <dylanromansky@yahoo.com>
2022-03-17 17:40:39 -0700

    implemented a temporary workaround to solve a mild scrape issue. I will be re-implementing the linkedin scrape in selenium which should fix all the intermittent failures caused by scraping a page that sometimes hasn't fully loaded by the time the scraper gets the information

--- scraping/linkedin.py
+++ scraping/linkedin.py
@@ -8,6 +8,12 @@ import re
 
 #TODO: only save results if they've been posted since the
 #last time I scraped
+#---implemented temp workaround. url now only gets results
+#	from the last 24 hours
+
+#TODO: convert to selenium as beautifulsoup assumes the
+#entire page will be loaded when it starts scraping.
+#perfect opportunity to practice using git branches
 
 #TODO: make this script take the desired destination as an
 #argument
@@ -16,6 +22,8 @@ start_dir = '/home/dromansk/projects/Python/scraping/output/linkedin'
 
 #remove all false links from my scraping results
 def hidden_check(i, links):
+	if (links == [] or i > len(links)):
+		return
 	for classes in links[i].get('class'):
 		if re.search(".*hidden.*", classes):
 			del links[i]
@@ -28,7 +36,7 @@ def hidden_check(i, links):
 #paramters and have the function scrape from those results
 
 def crawl_linkedin():
-	search = BeautifulSoup(requests.get("https://www.linkedin.com/jobs/search/?f_E=1%2C2&f_JT=F%2CP%2CI&geoId=102095887&keywords=sre&location=California%2C%20United%20States&sortBy=DD").text, 'html.parser')
+	search = BeautifulSoup(requests.get("https://www.linkedin.com/jobs/search/?f_E=1%2C2&f_JT=F%2CP%2CI&f_TPR=r86400&geoId=102095887&keywords=sre&location=California%2C%20United%20States&sortBy=DD").text, 'html.parser')
 	links = search.find('ul', class_='jobs-search__results-list')
 	links = links.find_all('a')
 	os.chdir(start_dir)
@@ -38,15 +46,25 @@ def crawl_linkedin():
 		os.mkdir(str(date.today()))
 	os.chdir(str(date.today()))
 	for i in range(0, 20, 1):
-		hidden_check(i, links)
-		link = links[i].get('href')
-		desc = BeautifulSoup(requests.get(link).text, 'html.parser')
-		title = desc.select("#main-content > section.core-rail > div > section.top-card-layout > div > div.top-card-layout__entity-info-container > div > h1")
-		desc = desc.find('div', class_='show-more-less-html__markup')
-		filename = str(i + 1).zfill(2) + ' ' + title[0].string + ".txt"
-		print("creating: " + filename)
-		file = open(filename, 'w')
-		file.write(link + '\n\n')
-		for string in desc.stripped_strings:
-			file.write(string + '\n')
-		file.close
+		for attempt in range(100):
+			try:
+				hidden_check(i, links)
+				if (links == [] or not links[i]):
+					return
+				link = links[i].get('href')
+				desc = BeautifulSoup(requests.get(link).text, 'html.parser')
+				title = desc.select("#main-content > section.core-rail > div > section.top-card-layout > div > div.top-card-layout__entity-info-container > div > h1")
+				if (not title):
+					title = desc.select("body > div.application-outlet > div.authentication-outlet > div > div.job-view-layout.jobs-details > div.grid > div > div:nth-child(1) > div > div.p5 > h1")
+				desc = desc.find('div', class_='show-more-less-html__markup')
+				filename = str(i + 1).zfill(2) + ' ' + title[0].string + ".txt"
+				filename = filename.replace('/', ' -or- ')
+				print("creating: " + filename)
+				file = open(filename, 'w')
+				file.write(link + '\n\n')
+				for string in desc.stripped_strings:
+					file.write(string + '\n')
+				file.close
+				break
+			except:
+				continue
