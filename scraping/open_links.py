3d16c6d3f032512470160ef6adac62209c83c255
bigdiggysquiggle <dylanromansky@yahoo.com>
2022-03-22 21:25:28 -0700

    open_links.py no longer waits for the whole page to load before opening the next tab, as the goal of the script is to open every scraped link so it's pointless for it to wait for a complete page load

--- scraping/open_links.py
+++ scraping/open_links.py
@@ -4,14 +4,16 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import chrome_start
 import sys
 
 def user_input():
 	print('Filenames:')
 	return input().split(' ')
-
-driver = chrome_start.chrome_start()
+caps = DesiredCapabilities.CHROME
+caps["pageLoadStrategy"] = "none"
+driver = chrome_start.chrome_start(caps)
 if (len(sys.argv) < 2):
 	file_set = user_input()
 else:
